2175f805b995e49661d897a07c0744c2061d723d
sivanargana <siva.nargana66@gmail.com>
2025-04-26 18:43:56 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/scrape', async (req, res) => {
+  
+    const browser = await puppeteer.launch({ headless: false });  
+    const page = await browser.newPage();
+    await page.goto("https://www.example.com/", { waitUntil: 'domcontentloaded' });
+
+    res.send("hi")
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
