111c3593c380f8be7ff102a2c2f90862d3c03e22
sivanargana <siva.nargana66@gmail.com>
2025-04-26 19:14:38 +0530

    changes

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const PORT = 3000;
 
 app.get('/scrape', async (req, res) => {
   
+try{
     const browser = await puppeteer.launch({ headless: true });  
     const page = await browser.newPage();
+    await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
     await page.goto("https://www.example.com/", { waitUntil: 'domcontentloaded' });
 
+}catch(err){
+    console.log(err)
+}
     res.send("hi")
 
 });
